8b01ee7f6f0d5f0786fa3649fa6befba47333802
Basvashri Majge <majge_b@xoriant.com>
2025-02-21 10:59:31 +0530

    Basvashri code

--- src/Demo.java
+++ src/Demo.java
@@ -1,11 +1,6 @@
-public class Main {
+public class Demo {
     public static void main(String[] args) {
-        int x = 5;
-        int y = 6;
-        int sum = x + y;
-        System.out.println(sum);
-
-
+       
         int a = 15;
         int b = 5;
         int sub = a - b;
